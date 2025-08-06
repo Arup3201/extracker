8131cca8afe72462a27b551891d7f81cc309611a
Arup3201 <arupjana7365@gmail.com>
2025-08-06 19:36:07 +0530

    fix: syntax error in create-table query

--- api/scripts/create-tables.ts
+++ api/scripts/create-tables.ts
@@ -1,6 +1,17 @@
 import db from "../configs/db";
 
 async function createTables() {
+  // categories table
+  await db.query(`
+        CREATE TABLE categories(
+            id VARCHAR(150) PRIMARY KEY, 
+            created_at TIMESTAMP WITH TIME ZONE NOT NULL, 
+            updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+            name VARCHAR(150) NOT NULL
+        )
+    `);
+
+  // expenses table
   await db.query(`
         CREATE TABLE expenses(
             id VARCHAR(150) PRIMARY KEY, 
@@ -9,15 +20,7 @@ async function createTables() {
             amount DECIMAL(2) NOT NULL, 
             category VARCHAR(150) REFERENCES categories(id), 
             expense_at TIMESTAMP WITH TIME ZONE NOT NULL,
-            note TEXT, 
-        )
-    `);
-  await db.query(`
-        CREATE TABLE categories(
-            id VARCHAR(150) PRIMARY KEY, 
-            created_at TIMESTAMP WITH TIME ZONE NOT NULL, 
-            updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-            name VARCHAR(150) NOT NULL,
+            note TEXT
         )
     `);
 }
