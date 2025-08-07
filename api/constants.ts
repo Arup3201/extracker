22071a2a1f20f5a07db9fb92dc371ac50531d0a0
Arup3201 <arupjana7365@gmail.com>
2025-08-07 13:05:04 +0530

    update: api root and version constants

---
+++ api/constants.ts
@@ -0,0 +1,6 @@
+const API_ROOT = "/api";
+const VERSION = "v1";
+
+const API_PATH = `${API_ROOT}/${VERSION}/`;
+
+export { API_PATH };
--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
 import { ENV } from "./configs/env";
+import { API_PATH } from "./constants";
+import expenseRouter from "./routes/expenses";
 
 const app = express();
 const PORT = ENV.PORT || 8000;
+
+app.use(express.json());
+app.use(API_PATH, expenseRouter);
 
 app.listen(PORT, (err) => {
   if (err) {
