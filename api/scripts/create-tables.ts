cdc0eec2eb6d59c253cf5d832104e77c75b2c7d2
Arup3201 <arupjana7365@gmail.com>
2025-08-12 11:34:39 +0530

    fix: expenses category reference category value which is unique

--- api/scripts/create-tables.ts
+++ api/scripts/create-tables.ts
@@ -7,8 +7,8 @@ async function createTables() {
             id VARCHAR(150) PRIMARY KEY, 
             created_at TIMESTAMP WITH TIME ZONE NOT NULL, 
             updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-            name VARCHAR(150) NOT NULL, 
-            value VARCHAR(150) NOT NULL
+            name VARCHAR(150) UNIQUE NOT NULL, 
+            value VARCHAR(150) UNIQUE NOT NULL
         )
     `);
 
@@ -19,7 +19,7 @@ async function createTables() {
             created_at TIMESTAMP WITH TIME ZONE NOT NULL, 
             updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
             amount DECIMAL(2) NOT NULL, 
-            category VARCHAR(150) REFERENCES categories(id), 
+            category VARCHAR(150) REFERENCES categories(value), 
             expense_at TIMESTAMP WITH TIME ZONE NOT NULL,
             note TEXT
         )
